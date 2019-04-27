2105213674b4e3bcde705a545a6a324ea15e08e0
SamSamaa <sam.adib@free.fr>
2019-04-27 21:16:12 +0200

    custom nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Home from './Components/Home/Home';
 import Prevision from './Components/Prevision/Prevision';
