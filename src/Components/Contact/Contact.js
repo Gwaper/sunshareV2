a2b5442cdd20ccf7d5eaa107ea956252fac44082
SamSamaa <sam.adib@free.fr>
2019-04-28 15:33:37 +0200

    contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Contact.css';
+import { Button, Icon } from 'semantic-ui-react'
 
 export default function Contact() {
   return (
     <div className='contactGrid'>
-      <h2>Contact 10%</h2>
-      <h1>80%</h1>
-      <h2>10%</h2>
+      <div>
+        <img src='http://sunshare.fr/assets/images/logo_snsr.png' alt='logo' />
+        <div className='btnContact'><a href='https://www.sunshare.fr/contact'>
+        <Button  icon labelPosition='left'>
+          <Icon name='mail outline' />
+          Nous Contacter
+        </Button></a></div>
+      </div>
+      <p>Creative Common: OKET Team</p>
     </div>
   )
 }
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Home.css';
 import MeteoDay from '../MeteoDay/MeteoDay';
+import Graphome from './Graphom/Graphome';
 
 export default function Home () {
   return (      
     <div className='gridHome'>
-      <div className="graphHome"></div>
+      <div className="graphHome">
+        <Graphome className='zizi'/>
+      </div>
       <div className="meteoHome">
-        <MeteoDay className=""/>
+        <MeteoDay/>
       </div>
       <div className="infoHome"></div>
     </div>
